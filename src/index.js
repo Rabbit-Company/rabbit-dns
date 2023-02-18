2aab9384ba3c4b1c4e0ec084065e64aeee61302a
Ziga <ziga.zajc007@gmail.com>
2023-02-18 23:15:29 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -1,50 +1,25 @@
 import { Hono } from 'hono';
+import { cors } from 'hono/cors';
 const router = new Hono();
-const cache = caches.default;
-
-async function generateHash(message){
-	const msgUint8 = new TextEncoder().encode(message);
-	const hashBuffer = await crypto.subtle.digest('SHA-256', msgUint8);
-	const hashArray = Array.from(new Uint8Array(hashBuffer));
-	return hashArray.map((b) => b.toString(16).padStart(2, '0')).join('');
-}
-
-async function setValue(key, value, cacheTime = 60){
-	let cacheKey = "https://dns.rabbitdns.org?key=" + key;
-	let nres = new Response(value);
-	nres.headers.append('Cache-Control', 's-maxage=' + cacheTime);
-	await cache.put(cacheKey, nres);
-}
-
-async function getValue(key){
-	let value = null;
-	let cacheKey = "https://dns.rabbitdns.org?key=" + key;
-	let res = await cache.match(cacheKey);
-	if(res) value = await res.text();
-	return value;
-}
 
 async function processWireFormat(query){
-	const hash = await generateHash(query);
-	let value = await getValue('wire-' + hash);
-	if(value !== null) return new Response(value, { headers: { 'Content-Type': 'application/dns-message', 'Access-Control-Allow-Origin': '*' } });
 	const message = await fetch('https://cloudflare-dns.com/dns-query' + query, { headers: { 'Accept': 'application/dns-message' } });
 	if(message.status !== 200) return new Response(null, { status: message.status });
-	const [v1, v2] = message.body.tee();
-	await setValue('wire-' + hash, v1);
-	return new Response(v2, { headers: { 'Content-Type': 'application/dns-message', 'Access-Control-Allow-Origin': '*' } });
+	return new Response(message.body, { headers: { 'Content-Type': 'application/dns-message', 'Access-Control-Allow-Origin': '*' } });
 }
 
 async function processJsonFormat(query){
-	const hash = await generateHash(query);
-	let value = await getValue('json-' + hash);
-	if(value !== null) return new Response(value, { headers: { 'Content-Type': 'application/dns-json', 'Access-Control-Allow-Origin': '*' } });
 	const message = await fetch('https://cloudflare-dns.com/dns-query' + query, { headers: { 'Accept': 'application/dns-json' } });
 	if(message.status !== 200) return new Response(null, { status: message.status });
-	const [v1, v2] = message.body.tee();
-	await setValue('json-' + hash, v1);
-	return new Response(v2, { headers: { 'Content-Type': 'application/dns-json', 'Access-Control-Allow-Origin': '*' } });
+	return new Response(message.body, { headers: { 'Content-Type': 'application/dns-json', 'Access-Control-Allow-Origin': '*' } });
 }
+
+router.use('*', cors({
+		origin: '*',
+		allowHeaders: ['*'],
+		allowMethods: ['POST', 'GET', 'OPTIONS']
+	})
+);
 
 router.get('/dns-query', async request => {
 	const query = new URL(request.req.url).search;
@@ -55,14 +30,9 @@ router.get('/dns-query', async request => {
 
 router.post('/dns-query', async request => {
 	const query = await request.req.arrayBuffer();
-	const hash = await generateHash(new TextDecoder().decode(query));
-	let value = await getValue('wirePost-' + hash);
-	if(value !== null) return new Response(value, { headers: { 'Content-Type': 'application/dns-message', 'Access-Control-Allow-Origin': '*'} });
 	const message = await fetch('https://cloudflare-dns.com/dns-query', { method: 'POST', headers: { 'Content-Type': 'application/dns-message' }, body: query });
 	if(message.status !== 200) return new Response(null, { status: message.status });
-	const [v1, v2] = message.body.tee();
-	await setValue('wirePost-' + hash, v1);
-	return new Response(v2, { headers: { 'Content-Type': 'application/dns-message', 'Access-Control-Allow-Origin': '*' } });
+	return new Response(message.body, { headers: { 'Content-Type': 'application/dns-message', 'Access-Control-Allow-Origin': '*' } });
 });
 
 export default router;
