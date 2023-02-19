cec35f9c292348d100a40aa3a7de7ca697bad9fa
Ziga <ziga.zajc007@gmail.com>
2023-02-19 21:18:55 +0100

    Update

--- src/index.js
+++ src/index.js
@@ -2,16 +2,24 @@ import { Hono } from 'hono';
 import { cors } from 'hono/cors';
 const router = new Hono();
 
-async function processWireFormat(query){
-	const message = await fetch('https://cloudflare-dns.com/dns-query' + query, { headers: { 'Accept': 'application/dns-message' } });
+async function processWireFormat(dnsProvider, query){
+	const message = await fetch(dnsProvider + query, { headers: { 'Accept': 'application/dns-message' } });
 	if(message.status !== 200) return new Response(null, { status: message.status });
 	return new Response(message.body, { headers: { 'Content-Type': 'application/dns-message', 'Access-Control-Allow-Origin': '*' } });
 }
 
-async function processJsonFormat(query){
-	const message = await fetch('https://cloudflare-dns.com/dns-query' + query, { headers: { 'Accept': 'application/dns-json' } });
+async function processJsonFormat(dnsProvider, query){
+	const message = await fetch(dnsProvider + query, { headers: { 'Accept': 'application/dns-json' } });
 	if(message.status !== 200) return new Response(null, { status: message.status });
 	return new Response(message.body, { headers: { 'Content-Type': 'application/dns-json', 'Access-Control-Allow-Origin': '*' } });
+}
+
+function getDnsProvider(url){
+	const hostname = new URL(url).hostname;
+	let dnsProvider = "https://cloudflare-dns.com/dns-query";
+	if(hostname === 'family.rabbitdns.org') dnsProvider = "https://family.cloudflare-dns.com/dns-query";
+	if(hostname === 'security.rabbitdns.org') dnsProvider = "https://security.cloudflare-dns.com/dns-query";
+	return dnsProvider;
 }
 
 router.use('*', cors({
@@ -24,13 +32,13 @@ router.use('*', cors({
 router.get('/dns-query', async request => {
 	const query = new URL(request.req.url).search;
 	let contentType = request.req.header('Accept') || 'application/dns-message';
-	if(contentType === 'application/dns-json') return await processJsonFormat(query);
-	return await processWireFormat(query);
+	if(contentType === 'application/dns-json') return await processJsonFormat(getDnsProvider(request.req.url), query);
+	return await processWireFormat(getDnsProvider(request.req.url), query);
 });
 
 router.post('/dns-query', async request => {
 	const query = await request.req.arrayBuffer();
-	const message = await fetch('https://cloudflare-dns.com/dns-query', { method: 'POST', headers: { 'Content-Type': 'application/dns-message' }, body: query });
+	const message = await fetch(getDnsProvider(request.req.url), { method: 'POST', headers: { 'Content-Type': 'application/dns-message' }, body: query });
 	if(message.status !== 200) return new Response(null, { status: message.status });
 	return new Response(message.body, { headers: { 'Content-Type': 'application/dns-message', 'Access-Control-Allow-Origin': '*' } });
 });
